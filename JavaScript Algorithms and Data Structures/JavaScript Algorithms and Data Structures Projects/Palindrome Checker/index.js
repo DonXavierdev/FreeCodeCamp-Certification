90721aa8f18c8f96410f63e3ca96d582e5a25192
Don Xavier <mail.donxavier@gmail.com>
2023-06-13 21:37:47 +0530

    modified palindrome checker

--- JavaScript Algorithms and Data Structures/JavaScript Algorithms and Data Structures Projects/Palindrome Checker/index.js
+++ JavaScript Algorithms and Data Structures/JavaScript Algorithms and Data Structures Projects/Palindrome Checker/index.js
@@ -1,11 +1,11 @@
-function palindrome(str) {
-
-    str = str.toLowerCase().replace(/[\W_]/g, '');
-    for(var i = 0, len = str.length - 1; i < len/2; i++) {
-      if(str[i] !== str[len-i]) {
-        return false;
-      }
-    }
-    return true;
+function palindrome(str){
+  let out='';
+  for(let i=0;i<str.length;i++){
+    if(str[i].match(/[0-9a-z]/i)){
+      out+=str[i];
+    } 
   }
-  console.log(palindrome("Racecar"));
+  out=out.toLowerCase();
+  return out==out.split('').reverse().join('');
+}
+console.log(palindrome('1 eye for of 1 eye.'))
